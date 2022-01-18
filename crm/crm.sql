5a9a84929588fb14fee67bf0a857e9216860cef7
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-01-18 16:37:50 +0000

    adds tables

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,53 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_memberships;
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email_address TEXT,
+  phone_number TEXT,
+  company_id INTEGER,
+  title TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contacts_id INTEGER,
+  salespersons_id INTEGER,
+  interaction_type TEXT,
+  date TEXT,
+  notes TEXT
+);
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email_address TEXT,
+  phone_number TEXT, 
+  title TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE industry_memberships (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  industry_id INTEGER
+);
--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,31 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  name TEXT, 
+  description TEXT
+);
+
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  first_name TEXT, 
+  last_name TEXT, 
+  bio TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  time TEXT,
+  course_id INT,
+  teacher_id INT 
+);
+
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  student_id INT, 
+  section_id INT
+);
+
+
 
